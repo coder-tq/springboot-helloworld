bd098926aa9c4859124815e7cffe954db9172b0b
coder_tq <909413805@qq.com>
2021-03-15 21:32:49 +0800

    添加自定义MVCconfig

--- src/main/java/com/codertq/helloworld/config/MvcConfig.java
+++ src/main/java/com/codertq/helloworld/config/MvcConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
+
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginInterceptor()).addPathPatterns("/testInterceptor");
